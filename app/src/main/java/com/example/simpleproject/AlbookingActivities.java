5061ae875ac23299d160f447f432953ebd7c1cdb
Shouda Ren <2521509R@student.gla.ac.uk>
2022-02-09 09:10:58 +0000

    application initial

---
+++ app/src/main/java/com/example/simpleproject/AlbookingActivities.java
@@ -0,0 +1,14 @@
+package com.example.simpleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbookingActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_albooking_activities);
+    }
+}
---
+++ app/src/main/java/com/example/simpleproject/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.simpleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button seeAll,currentBook,already,wishlisht,about;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialView();
+        seeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AlbookingActivities.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialView() {
+        seeAll = findViewById(R.id.seeAll);
+        currentBook = findViewById(R.id.currentBook);
+        already = findViewById(R.id. already);
+        wishlisht = findViewById(R.id.wishlisht);
+        about = findViewById(R.id.about);
+
+    }
+}
